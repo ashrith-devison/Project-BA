9b79ff57b8e3384de3e4f81b87238b23c35ff85d
ashrith-devison <ashrithsai.devison@gmail.com>
2024-07-26 23:38:51 +0530

    Hidden the Admin and Tester Codes

--- admin/allocation/shift-allocation.js
+++ admin/allocation/shift-allocation.js
@@ -69,49 +69,49 @@ function relocate(department){
 }
 
 function print_list() {
-    Swal.fire({
-        title : 'Printing the List',
-        html : '<img src="/loading-gif.gif">',
-        showConfirmButton : false,
-        allowOutsideClick : false,
-        allowEscapeKey : false,
-        allowEnterKey : false,
-        didOpen : () => {
-            Swal.showLoading();
-        }
-    })
-    const content = document.getElementById('alloted-data-randomizer');
-    const department = $('#department-selection option:selected').text(); 
-    const currentDate = new Date().toISOString().slice(0,10);
-    const filename = `${currentDate}_${department}.pdf`; 
+    // Swal.fire({
+    //     title : 'Printing the List',
+    //     html : '<img src="/loading-gif.gif">',
+    //     showConfirmButton : false,
+    //     allowOutsideClick : false,
+    //     allowEscapeKey : false,
+    //     allowEnterKey : false,
+    //     didOpen : () => {
+    //         Swal.showLoading();
+    //     }
+    // })
+    // const content = document.getElementById('alloted-data-randomizer');
+    // const department = $('#department-selection option:selected').text(); 
+    // const currentDate = new Date().toISOString().slice(0,10);
+    // const filename = `${currentDate}_${department}.pdf`; 
 
-    html2canvas(content).then(canvas => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('l', 'mm', 'a4');
-        const pageWidth = pdf.internal.pageSize.getWidth();
-        const pageHeight = pdf.internal.pageSize.getHeight();
-        const imageWidth = canvas.width;
-        const imageHeight = canvas.height;
-        const imageRatio = imageWidth / imageHeight;
-        const pageRatio = pageWidth / pageHeight;
-        let scaledWidth, scaledHeight, xPosition, yPosition;
+    // html2canvas(content).then(canvas => {
+    //     const imgData = canvas.toDataURL('image/png');
+    //     const pdf = new jsPDF('l', 'mm', 'a4');
+    //     const pageWidth = pdf.internal.pageSize.getWidth();
+    //     const pageHeight = pdf.internal.pageSize.getHeight();
+    //     const imageWidth = canvas.width;
+    //     const imageHeight = canvas.height;
+    //     const imageRatio = imageWidth / imageHeight;
+    //     const pageRatio = pageWidth / pageHeight;
+    //     let scaledWidth, scaledHeight, xPosition, yPosition;
     
-        if (imageRatio > pageRatio) {
-            scaledWidth = pageWidth;
-            scaledHeight = pageWidth / imageRatio;
-            xPosition = 0;
-            yPosition = (pageHeight - scaledHeight) / 2; // Center vertically
-        } else {
-            scaledHeight = pageHeight;
-            scaledWidth = pageHeight * imageRatio;
-            xPosition = (pageWidth - scaledWidth) / 2; // Center horizontally
-            yPosition = 0;
-        }
+    //     if (imageRatio > pageRatio) {
+    //         scaledWidth = pageWidth;
+    //         scaledHeight = pageWidth / imageRatio;
+    //         xPosition = 0;
+    //         yPosition = (pageHeight - scaledHeight) / 2; // Center vertically
+    //     } else {
+    //         scaledHeight = pageHeight;
+    //         scaledWidth = pageHeight * imageRatio;
+    //         xPosition = (pageWidth - scaledWidth) / 2; // Center horizontally
+    //         yPosition = 0;
+    //     }
     
-        pdf.addImage(imgData, 'PNG', xPosition, yPosition, scaledWidth, scaledHeight);
-        pdf.save(filename);
-        Swal.close();
-    });
+    //     pdf.addImage(imgData, 'PNG', xPosition, yPosition, scaledWidth, scaledHeight);
+    //     pdf.save(filename);
+    //     Swal.close();
+    // });
     console.log("Printed");
     $('#generate-button-randomizer').show();
 }
